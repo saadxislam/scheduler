e3ba97b9c2b16935d5b5e235f6a64c6850076bc0
Muhammad Behlul Islam Saad <Saad@SI-MBP.local>
2020-10-28 15:53:40 -0700

    no longer using hardcoded days. set up axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,14 +1,9 @@
-// import React, { useState }  from "react";
-// import DayList from "components/DayList";
-// import "components/Application.scss";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-// // import Header from 'components/Header'
-// // import Show from 'components/Show'
-// // import Empty from 'components/Empty'
-// import Appointment from 'components/Appointment';
-
-// // import "components/Appointment.scss";
-
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+import "components/Application.scss";
 
 // const days = [
 //   {
@@ -27,112 +22,6 @@
 //     spots: 0,
 //   },
 // ];
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Kourtney Huget",
-//       interviewer: {
-//         id: 2,
-//         name: "Ralph Lauren",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Paige Lindahl",
-//       interviewer: {
-//         id: 3,
-//         name: "Tommy Hilfigher",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   {
-//     id: 6,
-//     time: "5pm",
-//   },
-// ];
-
-
-// export default function Application(props) {
-//   const [day, setDay] = useState("Monday");
-//   return (
-//     <main className="layout">
-//       <section className="sidebar">
-//         <img
-//   className="sidebar--centered"
-//   src="images/logo.png"
-//   alt="Interview Scheduler"
-// />
-// <hr className="sidebar__separator sidebar--centered" />
-// <nav className="sidebar__menu"> 
-//   <DayList days={days} day={day} setDay={setDay} />
-// </nav>
-// <img
-//   className="sidebar__lhl sidebar--centered"
-//   src="images/lhl.png"
-//   alt="Lighthouse Labs"
-// />
-//       </section>
-//       <section className="schedule">
-//         {appointments.map((appointment) => {
-//           return <Appointment time={props.time} {...appointment} />;
-//         })}
-//         <Appointment key="last" time="5pm" />
-//       </section>
-//     </main>
-//   );
-// }
-
-
-import React, { useState } from "react";
-import DayList from "components/DayList";
-import Appointment from "components/Appointment";
-import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -185,8 +74,19 @@ const appointments = [
   },
 ];
 
+
+
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      console.log(response.data);
+    })
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -197,7 +97,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={days} day={day} setDay={setDays} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
